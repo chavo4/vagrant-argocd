1cc7d183a6039172889442255be6c56de9839e5c
Chavdar Rakov <chavdar.rakov@gmail.com>
2025-01-03 21:16:12 +0200

    Update application.sh

--- scripts/application.sh
+++ scripts/application.sh
@@ -7,7 +7,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # GitHub repository URL
-REPO_URL="https://github.com/chavo1/tetris-argocd.git"
+REPO_URL="https://github.com/chavo1/vagrant-argocd.git"
 
 # Function to print messages
 print_message() {
